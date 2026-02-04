301ea6b4e953e669c714e1ca336d4e49b997e41b
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-02-04 14:31:00 +0100

    ERS-49 Set up logging & global error handling

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { AllExceptionsFilter } from './common/interceptors/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Apply global interceptor & filter BEFORE listening
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   console.log(`🚀 API running on http://localhost:${PORT}`);
