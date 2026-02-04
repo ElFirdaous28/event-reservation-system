0a69f0aa8ac55e86d19b721be4b063cd3987de3b
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-02-04 13:33:04 +0100

    ERS-48 Configure database connection & env variables

---
+++ packages/shared/src/enums.ts
@@ -0,0 +1,17 @@
+export enum Role {
+    ADMIN = 'ADMIN',
+    PARTICIPANT = 'PARTICIPANT',
+}
+
+export enum EventStatus {
+    DRAFT = 'DRAFT',
+    PUBLISHED = 'PUBLISHED',
+    CANCELED = 'CANCELED',
+}
+
+export enum ReservationStatus {
+    PENDING = 'PENDING',
+    CONFIRMED = 'CONFIRMED',
+    REFUSED = 'REFUSED',
+    CANCELED = 'CANCELED',
+}
